8ad44e8ea787af595bbca53650dd143101ceb56d
Egor Druzhinin <druzhinin.egor@gmail.com>
2018-09-24 03:34:36 +0700

    fall 2018

--- leagues/high/dates.js
+++ leagues/high/dates.js
@@ -1,32 +1,32 @@
 var dataItems = [
     {
         tour: 1,
-        day: 18,
-        month: 2,
+        day: 23,
+        month: 9,
         year: 2018
     },
     {
         tour: 2,
-        day: 4,
-        month: 3,
+        day: 14,
+        month: 10,
         year: 2018
     },
     {
         tour: 3,
-        day: 25,
-        month: 3,
+        day: 4,
+        month: 11,
         year: 2018
     },
     {
         tour: 4,
-        day: 1,
-        month: 4,
+        day: 25,
+        month: 11,
         year: 2018
     },
     {
         tour: 5,
-        day: 15,
-        month: 4,
+        day: 2,
+        month: 12,
         year: 2018
     }
 ]
--- leagues/student/dates.js
+++ leagues/student/dates.js
@@ -1,26 +1,33 @@
-var dataItems = [
+let dataItems = [
     {
         tour: 1,
-        day: 17,
-        month: 2,
+        day: 30,
+        month: 9,
         year: 2018
     },
     {
         tour: 2,
-        day: 8,
-        month: 3,
+        day: 7,
+        month: 10,
         year: 2018
     },
     {
         tour: 3,
-        day: 7,
-        month: 4,
+        day: 11,
+        month: 11,
         year: 2018
     },
     {
         tour: 4,
-        day: 21,
-        month: 4,
+        day: 18,
+        month: 11,
         year: 2018
     },
-]
+    {
+        tour: 5,
+        day: 2,
+        month: 12,
+        year: 2018
+    },
+
+];
