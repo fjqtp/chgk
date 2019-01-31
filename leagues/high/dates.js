ddff3b479329993164161450b8859f19c59ceffb
fjqtp <druzhinin.egor@gmail.com>
2019-01-31 13:40:46 +0700

    Update dates.js

--- leagues/high/dates.js
+++ leagues/high/dates.js
@@ -1,32 +1,32 @@
 var dataItems = [
     {
         tour: 1,
-        day: 23,
-        month: 9,
-        year: 2018
+        day: 17,
+        month: 2,
+        year: 2019
     },
     {
         tour: 2,
-        day: 14,
-        month: 10,
-        year: 2018
+        day: 3,
+        month: 3,
+        year: 2019
     },
     {
         tour: 3,
-        day: 4,
-        month: 11,
-        year: 2018
+        day: 17,
+        month: 3,
+        year: 2019
     },
     {
         tour: 4,
-        day: 25,
-        month: 11,
-        year: 2018
+        day: 7,
+        month: 4,
+        year: 2019
     },
     {
         tour: 5,
-        day: 9,
-        month: 12,
-        year: 2018
+        day: 28,
+        month: 4,
+        year: 2019
     }
 ]
