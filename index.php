b722959a7f8feb2d176ba519c7ecf1262c35347f
Mergent <47741198+Mergent@users.noreply.github.com>
2019-03-22 13:02:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -55,6 +55,31 @@
                 </a>
             </li>
             <li>
+                <a href="leagues/student/league.html"
+                   style="display: block; color: inherit; text-decoration: none;">
+                    <div style="display: flex; align-items: center; width: 100%;">
+                        <div style="margin-right: 4px; width: 24px; flex-grow: 0; flex-shrink: 0; display: flex; align-items: center; justify-content: center; min-height: calc((1.5em + 3px) + 3px);">
+                            <div style="height: 24px; width: 24px; border-radius: 3px; display: flex; align-items: center; justify-content: center;">
+                                <svg width="100%" height="100%" viewBox="0 0 30 30"
+                                     style="fill: currentcolor; display: block; width: 22px; height: 22px; min-height: 22px; min-width: 22px;">
+                                    <g>
+                                        <path d="M16,1H4v28h22V11L16,1z M16,3.828L23.172,11H16V3.828z M24,27H6V3h8v10h10V27z M8,17h14v-2H8V17z M8,21h14v-2H8V21z M8,25h14v-2H8V25z">
+                                        </path>
+                                    </g>
+                                </svg>
+                            </div>
+                        </div>
+                        <div style="flex: 1 1 0px; min-width: 1px; padding-top: 3px; padding-bottom: 3px; padding-left: 2px; width: 1px;">
+                            <div style="display: flex; align-items: center;">
+                                <div style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+                                    <span style="font-weight: 500; border-bottom: 1px solid rgb(232, 232, 232);">Первая лига 2.0</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </li>
+            <li>
                 <a href="leagues/first/league.html" style="display: block; color: inherit; text-decoration: none;">
                     <div style="display: flex; align-items: center; width: 100%;">
                         <div style="margin-right: 4px; width: 24px; flex-grow: 0; flex-shrink: 0; display: flex; align-items: center; justify-content: center; min-height: calc((1.5em + 3px) + 3px);">
