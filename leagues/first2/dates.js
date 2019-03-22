236bebfdf0d18ace4965d2be76fd157f9fa3c902
Mergent <47741198+Mergent@users.noreply.github.com>
2019-03-22 13:13:37 +0700

    Create dates.js

---
+++ leagues/first2/dates.js
@@ -0,0 +1,33 @@
+let dataItems = [
+    {
+        tour: 1,
+        day: 16,
+        month: 2,
+        year: 2019
+    },
+    {
+        tour: 2,
+        day: 8,
+        month: 3,
+        year: 2019
+    },
+    {
+        tour: 3,
+        day: 23,
+        month: 3,
+        year: 2019
+    },
+    {
+        tour: 4,
+        day: 13,
+        month: 4,
+        year: 2019
+    },
+    {
+        tour: 5,
+        day: 27,
+        month: 4,
+        year: 2019
+    },
+
+];
