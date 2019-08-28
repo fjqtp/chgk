d5e9e977c4173bf38f0f2c5c6f78c0038335c31a
Mergent <47741198+Mergent@users.noreply.github.com>
2019-08-28 10:30:12 +0700

    Update dates.js

--- leagues/first/dates.js
+++ leagues/first/dates.js
@@ -1,32 +1,32 @@
 let dataItems = [
     {
         tour: 1,
-        day: 16,
-        month: 2,
+        day: 5,
+        month: 10,
         year: 2019
     },
     {
         tour: 2,
-        day: 8,
-        month: 3,
+        day: 19,
+        month: 10,
         year: 2019
     },
     {
         tour: 3,
-        day: 23,
-        month: 3,
+        day: 9,
+        month: 11,
         year: 2019
     },
     {
         tour: 4,
-        day: 13,
-        month: 4,
+        day: 16,
+        month: 11,
         year: 2019
     },
     {
         tour: 5,
-        day: 27,
-        month: 4,
+        day: 7,
+        month: 12,
         year: 2019
     },
 
