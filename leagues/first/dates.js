0ec7bb02cb2984d841ee9af95b2f72ffae5ea6e0
fjqtp <druzhinin.egor@gmail.com>
2019-09-30 01:47:41 +0700

    Update dates.js

--- leagues/first/dates.js
+++ leagues/first/dates.js
@@ -1,31 +1,31 @@
 let dataItems = [
     {
         tour: 1,
-        day: 5,
+        day: 6,
         month: 10,
         year: 2019
     },
     {
         tour: 2,
-        day: 19,
+        day: 20,
         month: 10,
         year: 2019
     },
     {
         tour: 3,
-        day: 9,
+        day: 10,
         month: 11,
         year: 2019
     },
     {
         tour: 4,
-        day: 16,
+        day: 17,
         month: 11,
         year: 2019
     },
     {
         tour: 5,
-        day: 7,
+        day: 8,
         month: 12,
         year: 2019
     },
