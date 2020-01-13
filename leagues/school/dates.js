271f826ac13437725e65c89db0b91014aace258d
fjqtp <druzhinin.egor@gmail.com>
2020-01-13 18:02:46 +0300

    Update dates.js

--- leagues/school/dates.js
+++ leagues/school/dates.js
@@ -1,44 +1,44 @@
 var dataItems = [
     {
         tour: 1,
-        day: 22,
+        day: 21,
         month: 9,
         year: 2018
     },
     {
         tour: 2,
-        day: 27,
+        day: 19,
         month: 10,
         year: 2018
     },
     {
         tour: 3,
-        day: 24,
+        day: 16,
         month: 11,
         year: 2018
     },
     {
         tour: 4,
-        day: 15,
+        day: 14,
         month: 12,
         year: 2018
     },
     {
         tour: 5,
-        day: 19,
+        day: 25,
         month: 1,
         year: 2019
     },
     {
         tour: 6,
-        day: 16,
-        month: 3,
+        day: 15,
+        month: 2,
         year: 2019
     },
     {
         tour: 7,
-        day: 13,
-        month: 4,
+        day: 7,
+        month: 3,
         year: 2019
     }
 ]
